b3a854b35746b57635249286b455389a9536228a
Jesse Kinkead <jesse.kinkead@gmail.com>
2015-09-11 13:42:23 -0700

    Add back in Codec support, and memory map support.

--- core/src/main/scala/org/allenai/common/ChannelSource.scala
+++ core/src/main/scala/org/allenai/common/ChannelSource.scala
@@ -4,6 +4,8 @@ import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
 import java.util.NoSuchElementException
 
+import scala.io.Codec
+
 /** A class that combines the best elements of java.nio.channels.FileChannel and scala.io.Source.
   * This provides:
   * <ul>
@@ -12,29 +14,58 @@ import java.util.NoSuchElementException
   * <li>character iteration
   * <li>optional memory-mapping of files for fast reading
   * </ul>
-  * Memory-mapped files will be read in chunks of one gigabyte.
+  * Memory-mapped files will be read in chunks of one gigabyte. Memory mapping is only recommended
+  * for larger files, and may hurt performance for small files. It may also hurt performance for
+  * files larger than a gigabyte that are repositioned frequently, as calling `position(Long)` will
+  * result in a remap if the new position is in a different gigabyte segment.
   * @param inFile the file channel to wrap
   * @param bufferSize the size of the internal buffer to use
+  * @param memoryMap if true, map the file into memory
+  * @param codec the codec to use. Must be one of UTF-8 or ISO-8859-1.
   */
 class ChannelSource(
     inFile: FileChannel,
-    bufferSize: Int = 1024,
-    memoryMap: Boolean = true
-) extends Iterator[Char] {
+    memoryMap: Boolean = false,
+    bufferSize: Int = 1024
+)(implicit codec: Codec) extends Iterator[Char] {
   require(bufferSize >= 3, "Buffer must be at least 3 bytes to decode UTF-8!")
 
-  // TODO: Handle memoryMap parameter!!!
-  var inputRemaining = true
+  /** True if we're decoding as UTF-8; false if we're decoding as ISO-8859-1. */
+  val useUtf8 = if (codec.name == Codec.UTF8.name) {
+    true
+  } else if (codec.name == Codec.ISO8859.name) {
+    false
+  } else {
+    throw new IllegalArgumentException(s"Unsupported codec ${codec.name}")
+  }
 
-  val inBuffer = ByteBuffer.allocate(bufferSize)
-  inBuffer.limit(0)
+  /** True if the inFile still has data to read. Only updated if memoryMap is false. */
+  private[common] var inputRemaining = true
+
+  /** The position we are at in the fil. Only used if memoryMap is true. */
+  private[common] var totalOffset = 0l
+
+  /** The buffer to read input into and out of. Initialized to empty. */
+  private[common] var inBuffer = if (memoryMap) {
+    // To avoid the overhead of mapping in the first gigabyte of a file if we're just going to be
+    // seeking to a position, use an empty buffer here.
+    ByteBuffer.allocate(0)
+  } else {
+    val buffer = ByteBuffer.allocateDirect(bufferSize)
+    buffer.limit(0)
+    buffer
+  }
 
   override def hasNext: Boolean = inBuffer.hasRemaining || (inputRemaining && fillBuffer())
 
-  /** Reads and returns a single UTF-8 character from a the file. */
+  /** Reads and returns a single character from a the file. */
   override def next(): Char = {
     if (inBuffer.hasRemaining || (inputRemaining && fillBuffer())) {
-      nextUtf8
+      if (useUtf8) {
+        nextUtf8
+      } else {
+        nextIso8859
+      }
     } else {
       throw new NoSuchElementException("next() called on empty ChannelSource")
     }
@@ -44,46 +75,107 @@ class ChannelSource(
     * you to position past the end of the file, in which case no input will be read.
     */
   def position(newPosition: Long): Unit = {
-    inFile.position(newPosition)
-    inBuffer.limit(0)
-    inputRemaining = true
+    if (memoryMap) {
+      // Truncate the offset to the size of the file.
+      totalOffset = if (newPosition > inFile.size) { inFile.size } else { newPosition }
+      val size: Long = Math.min(inFile.size - totalOffset, ChannelSource.OneGigabyte)
+      inBuffer = inFile.map(FileChannel.MapMode.READ_ONLY, totalOffset, size)
+    } else {
+      inFile.position(newPosition)
+      inBuffer.limit(0)
+      inputRemaining = true
+    }
   }
 
   /** @return the current position in the file */
-  def position: Long = inFile.position - inBuffer.remaining
+  def position: Long = {
+    if (memoryMap) {
+      totalOffset + inBuffer.position
+    } else {
+      inFile.position - inBuffer.remaining
+    }
+  }
+
+  protected[common] def nextIso8859: Char = {
+    // `toChar` doesn't work as expected with bytes; they're first widened with sign extension,
+    // meaning that bytes with the high bit set expand into two-byte characters with the high byte
+    // set to 0xff. Mask into an int first to fix this.
+    (inBuffer.get() & 0xff).toChar
+  }
 
   protected[common] def nextUtf8: Char = {
     // Ensure we are either at the end of file, or have at least three bytes.
     if (inBuffer.remaining() < 3 && inputRemaining) {
       fillBuffer()
     }
+    // See https://en.wikipedia.org/wiki/UTF-8 for details. This is checking the first byte for the
+    // total length of the encoding.
     val first = inBuffer.get()
     val intVal = if ((first & 0x80) == 0) {
       return first.toChar
-    } else if (((first & 0xe0) ^ 0xc0) == 0) {
+    } else if ((first & 0xe0) == 0xc0) {
+      // TODO(jkinkead): Secondary bytes (here and below) should all have their two highest bits set
+      // as 10. This should be asserted somehow, and if they're corrupted, producing an unknown
+      // character instead.
       val second = inBuffer.get()
       ((first & 0x1f) << 6) | (second & 0x3f)
-    } else if (((first & 0xf0) ^ 0xe0) == 0) {
+    } else if ((first & 0xf0) == 0xe0) {
       val second = inBuffer.get()
       val third = inBuffer.get()
       ((first & 0x0f) << 12) | ((second & 0x3f) << 6) | (third & 0x3f)
     } else {
-      throw new Exception("JVM can only handle 16-bit unicode")
+      // Anything here is technically illegal UTF-8. However, some sources (i.e. Wikipedia) use
+      // wide UTF-8 encodings. This will attempt to skip the correct number of bytes. Note that
+      // while we could decode the character here, it will be wider than 16 bits, and therefore
+      // won't fit in a JVM Char.
+
+      // skipCount stores the *additional* bytes we will skip, not the total length of the encoding.
+      val skipCount = if ((first & 0xf8) == 0xf0) {
+        3
+      } else if ((first & 0xfc) == 0xf8) {
+        4
+      } else if ((first & 0xfe) == 0xfc) {
+        5
+      } else {
+        // This will be reached if the first character was truly illegal UTF-8, not just a wide
+        // encoding.
+        0
+      }
+      // Technically this is buggy if the buffer is 4 or 5 bytes - we won't skip the full character.
+      // That's fine.
+      if (inBuffer.remaining() < skipCount && inputRemaining) {
+        fillBuffer()
+      }
+      (0 until skipCount) foreach { _ => if (inBuffer.hasRemaining()) inBuffer.get() }
+      // Replace with the unknown character.
+      0xfffd
     }
     intVal.toChar
   }
 
   /** Fills inBuffer from the input file. Sets `inputRemaining` as appropriate.
-    * @return true if any characters are left in the file channel
+    * @return true if there are more characters to read in the buffer
     */
   protected[common] def fillBuffer(): Boolean = {
-    inBuffer.compact()
-    val numRead = inFile.read(inBuffer)
-    inputRemaining = numRead != -1
-    inBuffer.flip()
+    if (memoryMap) {
+      val currOffset = totalOffset + inBuffer.position
+      val size: Long = Math.min(inFile.size - currOffset, ChannelSource.OneGigabyte)
+      // Only remap if we have any more data to add.
+      if (size > inBuffer.remaining) {
+        totalOffset = currOffset
+        inBuffer = inFile.map(FileChannel.MapMode.READ_ONLY, totalOffset, size)
+      }
+      inBuffer.hasRemaining
+    } else {
+      inBuffer.compact()
+      val numRead = inFile.read(inBuffer)
+      inputRemaining = numRead != -1
+      inBuffer.flip()
 
-    inBuffer.hasRemaining
+      inBuffer.hasRemaining
+    }
   }
 }
 object ChannelSource {
+  val OneGigabyte: Long = 1L << 30
 }
--- core/src/test/scala/org/allenai/common/ChannelSourceSpec.scala
+++ core/src/test/scala/org/allenai/common/ChannelSourceSpec.scala
@@ -7,7 +7,12 @@ import java.nio.channels.FileChannel
 import java.nio.file.{ Files, StandardOpenOption }
 import java.util.NoSuchElementException
 
+import scala.io.Codec
+
 class ChannelSourceSpec extends UnitSpec {
+  // UTF-8 is the default encoding for these tests.
+  implicit val codec = Codec.UTF8
+
   /** @return a channel open to a new temp file containing the given bytes */
   def newFileWithBytes(bytes: Array[Byte]): FileChannel = {
     val path = Files.createTempFile(null, null)
@@ -25,20 +30,18 @@ class ChannelSourceSpec extends UnitSpec {
     channel
   }
 
-  "ChannelSource" should "read a simple file" in {
+  "ChannelSource" should "read a simple file in UT8" in {
     val foo = newFileWithBytes(Array('f', 'o', 'o') map { _.toByte })
 
     val source = new ChannelSource(foo)
-    source.hasNext should be(true)
-    source.next() should be('f')
-    source.hasNext should be(true)
-    source.next() should be('o')
-    source.hasNext should be(true)
-    source.next() should be('o')
-    source.hasNext should be(false)
-    intercept[NoSuchElementException] {
-      source.next()
-    }
+    source.mkString should be("foo")
+  }
+
+  it should "read a simple file in ISO-8859-1" in {
+    val foo = newFileWithBytes(Array('f', 'o', 'o') map { _.toByte })
+
+    val source = new ChannelSource(foo)(Codec.ISO8859)
+    source.mkString should be("foo")
   }
 
   it should "read a file in UTF-8 with a small buffer that crosses character boundaries" in {
@@ -48,21 +51,21 @@ class ChannelSourceSpec extends UnitSpec {
     // This buffer size will cross character boundaries (the first read will hit the middle of the
     // second ü).
     val source = new ChannelSource(foomlaut, bufferSize = 4)
-    source.mkString("") should be("füü")
+    source.mkString should be("füü")
   }
 
-  it should "report the correct byte position" in {
+  it should "report the correct byte position when reading wide chars" in {
     // Array stores füü, in UTF-8.
     val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
     val source = new ChannelSource(foomlaut, bufferSize = 4)
 
-    source.position should be(0)
+    withClue("Start position: ") { source.position should be(0) }
     source.next()
-    source.position should be(1)
+    withClue("After reading single-byte char: ") { source.position should be(1) }
     source.next()
-    source.position should be(3)
+    withClue("After reading two-byte char: ") { source.position should be(3) }
     source.next()
-    source.position should be(5)
+    withClue("After reading another two-byte char: ") { source.position should be(5) }
   }
 
   it should "allow positioning in a file" in {
@@ -71,11 +74,57 @@ class ChannelSourceSpec extends UnitSpec {
     val source = new ChannelSource(foomlaut, bufferSize = 4)
 
     source.position(3)
-    source.next() should be('ü')
-    source.position should be(5)
-    source.hasNext should be(false)
+    withClue("After scanning to final character: ") { source.next() should be('ü') }
+    withClue("After reading the final character: ") {
+      source.position should be(5)
+      source.hasNext should be(false)
+    }
 
     source.position(0)
-    source.mkString("") should be("füü")
+    withClue("After reading and resetting: ") { source.mkString("") should be("füü") }
+  }
+
+  it should "read high-valued ISO-8859-1 characters" in {
+    val catsSay = newFileWithBytes(Array(0xb5.toByte))
+    val source = new ChannelSource(catsSay)(Codec.ISO8859)
+
+    source.mkString("") should be("µ")
+  }
+
+  it should "read a simple memory-mapped file" in {
+    val foo = newFileWithBytes(Array('f', 'o', 'o') map { _.toByte })
+
+    val source = new ChannelSource(foo, memoryMap = true)
+    source.mkString should be("foo")
+  }
+
+  it should "report the correct position in a memory-mapped file" in {
+    // Array stores füü, in UTF-8.
+    val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
+    val source = new ChannelSource(foomlaut, memoryMap = true)
+
+    withClue("Start position: ") { source.position should be(0) }
+    source.next()
+    withClue("After reading single-byte char: ") { source.position should be(1) }
+    source.next()
+    withClue("After reading two-byte char: ") { source.position should be(3) }
+    source.next()
+    withClue("After reading another two-byte char: ") { source.position should be(5) }
+  }
+
+  it should "allow positioning in a memory-mapped file" in {
+    // Array stores füü, in UTF-8.
+    val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
+    val source = new ChannelSource(foomlaut, memoryMap = true)
+
+    source.position(3)
+    withClue("After scanning to final character: ") { source.next() should be('ü') }
+    withClue("After reading the final character: ") {
+      source.position should be(5)
+      source.hasNext should be(false)
+    }
+
+    source.position(0)
+    withClue("After reading and resetting: ") { source.mkString("") should be("füü") }
   }
 }
