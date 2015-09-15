f2a24d74ae82e2cb7ff8c2770bebdc1dd24d67f1
Jesse Kinkead <jesse.kinkead@gmail.com>
2015-09-15 11:30:59 -0700

    Functional ChannelSource.

--- core/src/main/scala/org/allenai/common/ChannelSource.scala
+++ core/src/main/scala/org/allenai/common/ChannelSource.scala
@@ -6,29 +6,32 @@ import java.util.NoSuchElementException
 
 import scala.io.Codec
 
-/** A class that combines the best elements of java.nio.channels.FileChannel and scala.io.Source.
-  * This provides:
-  * <ul>
-  * <li>byte-valued position in the file
-  * <li>seekability (seek-to-position)
-  * <li>character iteration
-  * <li>optional memory-mapping of files for fast reading
-  * </ul>
-  * Memory-mapped files will be read in chunks of one gigabyte. Memory mapping is only recommended
-  * for larger files, and may hurt performance for small files. It may also hurt performance for
-  * files larger than a gigabyte that are repositioned frequently, as calling `position(Long)` will
-  * result in a remap if the new position is in a different gigabyte segment.
+/** A class that provides seekability to an interface similar to scala.io.Source. This is required
+  * because scala.io.Source only provides character offsets, while FileChannel (and other seekable
+  * Java interfaces) take byte offsets for seeking. The only limitation is that the file must be
+  * either UTF-8 or ISO-8859-1 encoded.
+  * <p>This also has a `getLines()` method to iterate over lines. Any reads done on this iterator
+  * are reflected in the main Source, and reads on the Source are reflected in the
+  * `Iterator[String]`.</p>
   * @param inFile the file channel to wrap
   * @param bufferSize the size of the internal buffer to use
-  * @param memoryMap if true, map the file into memory
   * @param codec the codec to use. Must be one of UTF-8 or ISO-8859-1.
+  * @throws IllegalArgumentException if `bufferSize` is less than 3, or `codec` is not UTF-8 or
+  * ISO-8859-1
   */
-class ChannelSource(
-    inFile: FileChannel,
-    memoryMap: Boolean = false,
-    bufferSize: Int = 1024
-)(implicit codec: Codec) extends Iterator[Char] {
+class ChannelSource(inFile: FileChannel, bufferSize: Int = 8192)(implicit codec: Codec)
+    extends Iterator[Char] {
   require(bufferSize >= 3, "Buffer must be at least 3 bytes to decode UTF-8!")
+
+  // The motivation for this class is explained above, but some of the implementation choices are a
+  // little confusing. This has decoding implemented within the class below, which seems odd. The
+  // reason for this is that the Decoder interfaces in java.nio.charset don't provide feedback for
+  // the number of bytes consumed while decoding a character, and trying to infer this (say, by
+  // passing buffers of size 1, 2, 3, etc. until it successfully decodes the next character) are
+  // incredibly inefficient.
+  //
+  // This uses manual decoding of individual characters for the Iterator[Char], but uses builtin
+  // decoding for linewise reading for the Iterator[String] returned from getLines().
 
   /** True if we're decoding as UTF-8; false if we're decoding as ISO-8859-1. */
   val useUtf8 = if (codec.name == Codec.UTF8.name) {
@@ -39,23 +42,17 @@ class ChannelSource(
     throw new IllegalArgumentException(s"Unsupported codec ${codec.name}")
   }
 
-  /** True if the inFile still has data to read. Only updated if memoryMap is false. */
+  /** True if the inFile still has data to read. */
   private[common] var inputRemaining = true
 
-  /** The position we are at in the fil. Only used if memoryMap is true. */
-  private[common] var totalOffset = 0L
-
   /** The buffer to read input into and out of. Initialized to empty. */
-  private[common] var inBuffer = if (memoryMap) {
-    // To avoid the overhead of mapping in the first gigabyte of a file if we're just going to be
-    // seeking to a position, use an empty buffer here.
-    ByteBuffer.allocate(0)
-  } else {
+  private[common] var inBuffer = {
     val buffer = ByteBuffer.allocateDirect(bufferSize)
     buffer.limit(0)
     buffer
   }
 
+  /** Returns true if the source has more input, populating the input buffer if need be. */
   override def hasNext: Boolean = inBuffer.hasRemaining || (inputRemaining && fillBuffer())
 
   /** Reads and returns a single character from a the file. */
@@ -75,27 +72,15 @@ class ChannelSource(
     * you to position past the end of the file, in which case no input will be read.
     */
   def position(newPosition: Long): Unit = {
-    if (memoryMap) {
-      // Truncate the offset to the size of the file.
-      totalOffset = if (newPosition > inFile.size) { inFile.size } else { newPosition }
-      val size: Long = Math.min(inFile.size - totalOffset, ChannelSource.MaxMapSize)
-      inBuffer = inFile.map(FileChannel.MapMode.READ_ONLY, totalOffset, size)
-    } else {
-      inFile.position(newPosition)
-      inBuffer.limit(0)
-      inputRemaining = true
-    }
+    inFile.position(newPosition)
+    inBuffer.limit(0)
+    inputRemaining = true
   }
 
   /** @return the current position in the file */
-  def position: Long = {
-    if (memoryMap) {
-      totalOffset + inBuffer.position
-    } else {
-      inFile.position - inBuffer.remaining
-    }
-  }
+  def position: Long = inFile.position - inBuffer.remaining
 
+  /** @return the next character in the buffer, decoded from ISO-8859-1 */
   protected[common] def nextIso8859: Char = {
     // `toChar` doesn't work as expected with bytes; they're first widened with sign extension,
     // meaning that bytes with the high bit set expand into two-byte characters with the high byte
@@ -103,8 +88,9 @@ class ChannelSource(
     (inBuffer.get() & 0xff).toChar
   }
 
+  /** @return the next character in the buffer, decoded from UTF-8 */
   protected[common] def nextUtf8: Char = {
-    // Ensure we are either at the end of file, or have at least three bytes.
+    // Ensure we are either at the end of file, or have at least three bytes that are readable.
     if (inBuffer.remaining() < 3 && inputRemaining) {
       fillBuffer()
     }
@@ -157,115 +143,163 @@ class ChannelSource(
     * @return true if there are more characters to read in the buffer
     */
   protected[common] def fillBuffer(): Boolean = {
-    if (memoryMap) {
-      val currOffset = totalOffset + inBuffer.position
-      val size: Long = Math.min(inFile.size - currOffset, ChannelSource.MaxMapSize)
-      // Only remap if we have any more data to add.
-      if (size > inBuffer.remaining) {
-        totalOffset = currOffset
-        inBuffer = inFile.map(FileChannel.MapMode.READ_ONLY, totalOffset, size)
-      }
-      inBuffer.hasRemaining
-    } else {
-      inBuffer.compact()
-      val numRead = inFile.read(inBuffer)
-      inputRemaining = numRead != -1
-      inBuffer.flip()
+    inBuffer.compact()
+    val numRead = inFile.read(inBuffer)
+    inputRemaining = numRead != -1
+    inBuffer.flip()
 
-      inBuffer.hasRemaining
-    }
-  }
-
-  /** Iterator class for reading the file linewise. */
-  private[common] class LineIterator extends Iterator[String] {
-    val newline = '\n'.toByte
-    val charset = codec.charSet
-    override def hasNext: Boolean = ChannelSource.this.hasNext
-    var bufferSize = 1024
-    var lineBuffer: Array[Byte] = new Array(bufferSize)
-    var lineLength = 0
-
-    override def next(): String = {
-      if (!ChannelSource.this.hasNext) {
-        throw new NoSuchElementException("next() called with no lines remaining")
-      }
-      lineLength = 0
-      var b = inBuffer.get()
-      // Read chars until we find a newline.
-      while (b != newline && ChannelSource.this.hasNext) {
-        if (bufferSize == lineLength) {
-          bufferSize = bufferSize * 2
-          val newBuffer = new Array[Byte](bufferSize)
-          System.arraycopy(lineBuffer, 0, newBuffer, 0, lineBuffer.length)
-          lineBuffer = newBuffer
-        }
-        lineBuffer(lineLength) = b
-        lineLength += 1
-        b = inBuffer.get()
-      }
-      // Append final char if we reached the end-of-file.
-      if (b != '\n') {
-        if (bufferSize == lineLength) {
-          bufferSize = bufferSize * 2
-          val newBuffer = new Array[Byte](bufferSize)
-          System.arraycopy(lineBuffer, 0, newBuffer, 0, lineBuffer.length)
-          lineBuffer = newBuffer
-        }
-        lineBuffer(lineLength) = b
-        lineLength += 1
-      }
-      new String(lineBuffer, 0, lineLength, charset)
-    }
+    inBuffer.hasRemaining
   }
 
   /** Returns an iterator over the remaining lines in this ChannelSource. Note that any reads done
     * on this iterator will be reflected in the main ChannelSource!
+    * <p>This acts in the same way as java.io.BufferedReader or scala.io.Source: Single newlines or
+    * single carriage returns are treated as an end-of-line, as are carriage returns followed
+    * immediately by newlines.</p>
     */
   def getLines(): Iterator[String] = new LineIterator
+
+  /** Iterator class for reading the file linewise. This keeps a byte array that's a mirror of the
+    * ByteBuffer in the wrapping ChannelSource. This is scanned for encoded newlines and / or
+    * carriage returns - which we can do because we only support two encodings, both of which use
+    * the same single byte for these characters.
+    */
+  private[common] class LineIterator extends Iterator[String] {
+    // Byte values of newlin & carriage return.
+    val newline = '\n'.toByte
+    val carriageReturn = '\r'.toByte
+    val charset = codec.charSet
+
+    /** The buffer to read the file into, and create strings out of. */
+    private[common] val lineBuffer: Array[Byte] = new Array(bufferSize)
+    /** The current index into lineBuffer. Kept in sync with inBuffer.position. */
+    private[common] var index = 0
+    /** The index of the last valid byte in lineBuffer. Kept in sync with inBuffer.limit. */
+    private[common] var limit = 0
+
+    /** Fills the line buffer if it needs filling.
+      * @return true if there is still input available
+      */
+    private[common] def ensureFullBuffer(): Boolean = {
+      // If we're at the end of our lineBuffer, we need to read more from ChannelSource.
+      if (index == limit) {
+        // Repopulate inBuffer via hasNext.
+        if (ChannelSource.this.hasNext) {
+          val capacity = Math.min(inBuffer.remaining, lineBuffer.length)
+          val startPosition = inBuffer.position
+          // Copy data to lineBuffer, and reset our indexes.
+          inBuffer.get(lineBuffer, startPosition, capacity)
+          index = startPosition
+          limit = startPosition + capacity
+          inBuffer.position(startPosition)
+          true
+        } else {
+          false
+        }
+      } else {
+        true
+      }
+    }
+
+    override def hasNext: Boolean = ensureFullBuffer()
+
+    /** Returns the next line in inBuffer. */
+    override def next(): String = {
+      if (!ensureFullBuffer()) {
+        throw new NoSuchElementException("next() called with no lines remaining")
+      }
+      // Update our index + limit, in case the underlying ChannelSource has moved since we were last
+      // iterated on.
+      index = inBuffer.position
+      limit = inBuffer.limit
+      // Holds the string we're building the second and subsequent times through the loop below. If
+      // we find a newline contained entirely in the buffer, we'll just create a string directly.
+      var stringBuilder: StringBuilder = null
+      while (ensureFullBuffer()) {
+        var start = index
+
+        // Read chars until we find a newline or the end-of-buffer.
+        var ch: Byte = 0
+        var foundEol = false
+        while (index != limit && !foundEol) {
+          ch = lineBuffer(index)
+          index += 1
+          if (ch == newline || ch == carriageReturn) {
+            foundEol = true
+          }
+        }
+        val length = index - start
+
+        // Scan the parent's input buffer to the position we just read to.
+        inBuffer.position(index)
+
+        // We've reached the end of the buffer or the end of the line.
+        if (foundEol) {
+          // The string we've built. Remove the EOL.
+          val str = new String(lineBuffer, start, length - 1, charset)
+          // Skip a newline that follows a carriage return.
+          if (ch == carriageReturn && ensureFullBuffer() && lineBuffer(index) == newline) {
+            index += 1
+            inBuffer.position(index)
+          }
+          return if (stringBuilder == null) str else stringBuilder.append(str).toString
+        }
+
+        if (stringBuilder == null) {
+          stringBuilder = new StringBuilder(120)
+        }
+        stringBuilder.append(new String(lineBuffer, start, length, charset))
+      }
+      stringBuilder.toString
+    }
+  }
 }
 object ChannelSource {
   val MaxMapSize: Long = (1L << 31) - 1
   def main(args: Array[String]): Unit = {
-    println("Timing Source and ChannelSource.")
     val wikip = "/Users/Kinkead/data/wikipedia/Wikipedia-all.txt-v1"
 
-    def timeMappedChannelSource(): Unit = {
-      val startTime = System.currentTimeMillis
-      val inFile = FileChannel.open(java.nio.file.Paths.get(wikip))
-      val channelSource = new ChannelSource(inFile, memoryMap = true)
-      //val charCount = channelSource count { _ => true }
-      //println(s"MappedChannelSource charCount = $charCount")
-      val lineCount = channelSource count { _ == '\n' }
-      println(s"MappedChannelSource lineCount = $lineCount")
-      println(s"MappedChannelSource took ${System.currentTimeMillis - startTime} millis.")
-      inFile.close()
-    }
-
     def timeChannelSource(): Unit = {
-      val startTime = System.currentTimeMillis
+      println("Timing ChannelSource -")
+      var startTime = System.currentTimeMillis
       val inFile = FileChannel.open(java.nio.file.Paths.get(wikip))
-      val channelSource = new ChannelSource(inFile, bufferSize = 1 << 30)
-      //val charCount = channelSource count { _ => true }
-      //println(s"ChannelSource charCount = $charCount")
-      //val count = channelSource count { _ == '\n' }
-      val count = channelSource.getLines().size
-      println(s"ChannelSource count = $count")
+      val channelSource = new ChannelSource(inFile)
+      println("Timing char iterator line count . . .")
+      val charCount = channelSource count { _ == '\n' }
+      println(s"ChannelSource charCount = $charCount")
       println(s"ChannelSource took ${System.currentTimeMillis - startTime} millis.")
+
+      channelSource.position(0)
+      startTime = System.currentTimeMillis
+      println("Timing getLines line count . . .")
+      val getLinesCount = channelSource.getLines().size
+      println(s"ChannelSource getLinesCount = $getLinesCount")
+      println(s"ChannelSource took ${System.currentTimeMillis - startTime} millis.")
+
       inFile.close()
     }
 
     def timeSource(): Unit = {
-      val startTime = System.currentTimeMillis
-      val scalaSource = scala.io.Source.fromFile(wikip)
-      //val count = scalaSource count { _ == '\n' }
-      val count = scalaSource.getLines().size
-      println(s"Scala count = $count")
-      println(s"Scala took ${System.currentTimeMillis - startTime} millis.")
+      println("Timing scala.io.Source -")
+      var startTime = System.currentTimeMillis
+      var scalaSource = scala.io.Source.fromFile(wikip)
+      println("Timing char iterator line count . . .")
+      val charCount = scalaSource count { _ == '\n' }
+      println(s"Scala Source charCount = $charCount")
+      println(s"Scala Source took ${System.currentTimeMillis - startTime} millis.")
       scalaSource.close
+
+      scalaSource = scala.io.Source.fromFile(wikip)
+      startTime = System.currentTimeMillis
+      println("Timing getLines line count . . .")
+      val getLinesCount = scalaSource.getLines().size
+      println(s"Scala Source getLinesCount = $getLinesCount")
+      println(s"Scala Source took ${System.currentTimeMillis - startTime} millis.")
+
+      scalaSource.close()
     }
 
-    timeChannelSource()
     //timeSource()
-    //timeMappedChannelSource()
+    timeChannelSource()
   }
 }
--- core/src/test/scala/org/allenai/common/ChannelSourceSpec.scala
+++ core/src/test/scala/org/allenai/common/ChannelSourceSpec.scala
@@ -13,6 +13,14 @@ class ChannelSourceSpec extends UnitSpec {
   // UTF-8 is the default encoding for these tests.
   implicit val codec = Codec.UTF8
 
+  /** Stores füü, in UTF-8. */
+  val foomlaut: Array[Byte] = Array('f'.toByte, 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte }
+
+  /** @return a channel open to a new temp file containing the given chars */
+  def newFileWithChars(chars: Iterable[Char]): FileChannel = {
+    newFileWithBytes(chars.toArray map { _.toByte })
+  }
+
   /** @return a channel open to a new temp file containing the given bytes */
   def newFileWithBytes(bytes: Array[Byte]): FileChannel = {
     val path = Files.createTempFile(null, null)
@@ -31,33 +39,31 @@ class ChannelSourceSpec extends UnitSpec {
   }
 
   "ChannelSource" should "read a simple file in UT8" in {
-    val foo = newFileWithBytes(Array('f', 'o', 'o') map { _.toByte })
+    val foo = newFileWithChars("foo")
 
     val source = new ChannelSource(foo)
     source.mkString should be("foo")
   }
 
   it should "read a simple file in ISO-8859-1" in {
-    val foo = newFileWithBytes(Array('f', 'o', 'o') map { _.toByte })
+    val foo = newFileWithChars("foo")
 
     val source = new ChannelSource(foo)(Codec.ISO8859)
     source.mkString should be("foo")
   }
 
   it should "read a file in UTF-8 with a small buffer that crosses character boundaries" in {
-    // Array stores füü, in UTF-8.
-    val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
+    val testfile = newFileWithBytes(foomlaut)
 
     // This buffer size will cross character boundaries (the first read will hit the middle of the
     // second ü).
-    val source = new ChannelSource(foomlaut, bufferSize = 4)
+    val source = new ChannelSource(testfile, bufferSize = 4)
     source.mkString should be("füü")
   }
 
   it should "report the correct byte position when reading wide chars" in {
-    // Array stores füü, in UTF-8.
-    val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
-    val source = new ChannelSource(foomlaut, bufferSize = 4)
+    val testfile = newFileWithBytes(foomlaut)
+    val source = new ChannelSource(testfile, bufferSize = 4)
 
     withClue("Start position: ") { source.position should be(0) }
     source.next()
@@ -69,9 +75,8 @@ class ChannelSourceSpec extends UnitSpec {
   }
 
   it should "allow positioning in a file" in {
-    // Array stores füü, in UTF-8.
-    val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
-    val source = new ChannelSource(foomlaut, bufferSize = 4)
+    val testfile = newFileWithBytes(foomlaut)
+    val source = new ChannelSource(testfile, bufferSize = 4)
 
     source.position(3)
     withClue("After scanning to final character: ") { source.next() should be('ü') }
@@ -85,55 +90,67 @@ class ChannelSourceSpec extends UnitSpec {
   }
 
   it should "read high-valued ISO-8859-1 characters" in {
+    // Mu, encoded in ISO-8859-1.
     val catsSay = newFileWithBytes(Array(0xb5.toByte))
     val source = new ChannelSource(catsSay)(Codec.ISO8859)
 
     source.mkString("") should be("µ")
   }
 
-  it should "read a simple memory-mapped file" in {
-    val foo = newFileWithBytes(Array('f', 'o', 'o') map { _.toByte })
-
-    val source = new ChannelSource(foo, memoryMap = true)
-    source.mkString should be("foo")
-  }
-
-  it should "report the correct position in a memory-mapped file" in {
-    // Array stores füü, in UTF-8.
-    val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
-    val source = new ChannelSource(foomlaut, memoryMap = true)
-
-    withClue("Start position: ") { source.position should be(0) }
-    source.next()
-    withClue("After reading single-byte char: ") { source.position should be(1) }
-    source.next()
-    withClue("After reading two-byte char: ") { source.position should be(3) }
-    source.next()
-    withClue("After reading another two-byte char: ") { source.position should be(5) }
-  }
-
-  it should "allow positioning in a memory-mapped file" in {
-    // Array stores füü, in UTF-8.
-    val foomlaut = newFileWithBytes(Array('f', 0xc3, 0xbc, 0xc3, 0xbc) map { _.toByte })
-    val source = new ChannelSource(foomlaut, memoryMap = true)
-
-    source.position(3)
-    withClue("After scanning to final character: ") { source.next() should be('ü') }
-    withClue("After reading the final character: ") {
-      source.position should be(5)
-      source.hasNext should be(false)
-    }
-
-    source.position(0)
-    withClue("After reading and resetting: ") { source.mkString("") should be("füü") }
-  }
-
-  it should "read lines from a file" in {
-    val newlines = newFileWithBytes(Array('a', '\n', 'b') map { _.toByte })
+  "ChannelSource.getLines" should "read lines from a file" in {
+    val newlines = newFileWithChars("a\nb")
     val source = new ChannelSource(newlines)
     val lines = source.getLines()
     withClue("The first line: ") { lines.next() should be("a") }
-    withClue("After reading the first line: ") { source.position should be(2) }
+    withClue("Position after reading the first line: ") { source.position should be(2) }
     withClue("The second line: ") { lines.next() should be("b") }
+    withClue("Position after reading the second line: ") { source.position should be(3) }
+    lines.hasNext should be(false)
+  }
+
+  it should "handle lines that span buffer boundaries" in {
+    val newlines = newFileWithChars("abcde\nfghij\n")
+    val source = new ChannelSource(newlines, 4)
+    val lines = source.getLines()
+
+    lines.next() should be("abcde")
+    withClue("Position after reading the first line: ") { source.position should be(6) }
+    lines.next() should be("fghij")
+    withClue("Position after reading the second line: ") { source.position should be(12) }
+    lines.hasNext should be(false)
+  }
+
+  it should "handle empty lines" in {
+    val newlines = newFileWithChars("\n\na\n\n")
+    val source = new ChannelSource(newlines)
+    val lines = source.getLines()
+
+    lines.next() should be("")
+    lines.next() should be("")
+    lines.next() should be("a")
+    lines.next() should be("")
+    lines.hasNext should be(false)
+  }
+
+  it should "handle mixing getLines with getting chars" in {
+    val newlines = newFileWithChars("foo\nbar")
+    val source = new ChannelSource(newlines)
+    val lines = source.getLines()
+
+    source.next() should be('f')
+    lines.next() should be("oo")
+    source.next() should be('b')
+    lines.next() should be("ar")
+  }
+
+  it should "handle carriage returns correctly" in {
+    val windowsNewlines = newFileWithChars("foo\rbar\r\ngaz\r")
+    val source = new ChannelSource(windowsNewlines)
+    val lines = source.getLines()
+
+    lines.next() should be("foo")
+    lines.next() should be("bar")
+    lines.next() should be("gaz")
+    lines.hasNext should be(false)
   }
 }
