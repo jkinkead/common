8a0ae6cdebd05d6aaa2b3b96fde2259471722352
Jesse Kinkead <jessek@allenai.org>
2017-01-24 15:42:05 -0800

    Use Java 8's Closeable.

--- core/src/main/scala/org/allenai/common/Resource.scala
+++ core/src/main/scala/org/allenai/common/Resource.scala
@@ -1,23 +1,20 @@
 package org.allenai.common
 
+import java.io.Closeable
 import java.net.URL
 
+import scala.io.Source
+
 object Resource {
-  // Allow use of methods on structural types.
-  import scala.language.reflectiveCalls
-
-  type Closeable = { def close(): Unit }
-
-  /** A method for managing a simple resource.  After running a computation,
-    * the resource will be closed and the computation will be returned.
+  /** A method for managing a simple resource.  After running a computation, the resource will be
+    * closed and the computation will be returned.
     *
-    * More complicated resource management may warrant a dependency on
-    * "com.jsuereth" % "scala-arm", but this simple method handles most
-    * cases.
+    * More complicated resource management may warrant a dependency on "com.jsuereth" % "scala-arm",
+    * but this simple method handles most cases.
     *
-    * @param  resource  a closeable resource
-    * @param  f  a computation involving the supplied resource
-    * @return  the result of the computation over the resource
+    * @param resource a closeable resource
+    * @param f a computation involving the supplied resource
+    * @return the result of the computation over the resource
     */
   def using[A <: Closeable, B](resource: A)(f: A => B): B = {
     require(resource != null, "The supplied resource was null.")
@@ -42,10 +39,28 @@ object Resource {
     }
   }
 
+  /** A method for managing a Source.  After running a computation, the Source will be closed and
+    * the computation will be returned.
+    *
+    * This method exists because in Scala 2.11 Source doesn't extend java.io.Closeable.
+    *
+    * @param resource the source to auto-close
+    * @param f a computation involving the supplied resource
+    * @return the result of the computation over the resource
+    */
+  def using[B](resource: Source)(f: Source => B): B = {
+    require(resource != null, "The supplied resource was null.")
+    try {
+      f(resource)
+    } finally {
+      resource.close()
+    }
+  }
+
   /** Get a Java Resource.
     * This method provides a much nicer exception than the Java default (NPE).
     *
-    * @param  name  absolute path of the resource
+    * @param name absolute path of the resource
     */
   def getAbsolute(name: String): URL = {
     getOptAbsolute(name).getOrElse {
@@ -55,8 +70,8 @@ object Resource {
 
   /** Get a Java Resource, returning None if it could not be found.
     *
-    * @param  name  absolute path of the resource
-    * @returns  Some(URL) if the resource exists
+    * @param name absolute path of the resource
+    * @returns Some(URL) if the resource exists
     */
   def getOptAbsolute(name: String): Option[URL] = {
     Option(this.getClass.getClassLoader.getResource(name))
@@ -65,8 +80,8 @@ object Resource {
   /** Get a Java Resource.
     * This method provides a much nicer exception than the Java default (NPE).
     *
-    * @param  clazz  the class from which the resource may be relative
-    * @param  name  path of the resource (absolute if prefixed with /)
+    * @param clazz the class from which the resource may be relative
+    * @param name path of the resource (absolute if prefixed with /)
     */
   def get(clazz: Class[_], name: String): URL = {
     getOpt(clazz, name).getOrElse {
@@ -76,8 +91,8 @@ object Resource {
 
   /** Get a Java Resource, returning None if it could not be found.
     *
-    * @param  clazz  the class from which the resource may be relative
-    * @returns  Some(URL) if the resource exists
+    * @param clazz the class from which the resource may be relative
+    * @returns Some(URL) if the resource exists
     */
   def getOpt(clazz: Class[_], name: String): Option[URL] = {
     Option(clazz.getResource(name))
